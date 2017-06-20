2fa9edd6bfcdf4330dbdb072dc10ed38d338b499
hellobrittany <hello@brittanywetzel.com>
2017-06-19 21:34:13 -0500

    initial commit

---
+++ db/seeds.rb
@@ -0,0 +1,17 @@
+Person.create!([
+	      					 {
+	      						name: "Nick Wooster",
+	      						bio: "Most handsome silver fox." 
+	      						
+	      						}, 
+	      					{
+	      						name: "Steve Carrell",
+	      						bio: "Funnny silver fox."
+	      				
+	      						},
+	      					{
+	      						name: "Helena",
+	      						bio: "Not a silver fox." 
+	      						
+	      						}
+      					])
